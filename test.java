256a2ad275e76c2aba67e1db94ee34980229d7ec
Ella590 <112266204+Ella590@users.noreply.github.com>
2023-01-30 09:05:32 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,15 @@
+
+package practice;
+
+public class test {
+    public static void main(String[] args) {
+    java.util.Scanner num = new java.util.Scanner(System.in);
+    
+        
+    function sum(9,7) {
+    return 9+7;
+}
+        
+   
+}
+}
